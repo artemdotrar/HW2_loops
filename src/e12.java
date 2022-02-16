65281a7dbb18d4ddadddff1614cd450436c70447
artemdotrar <artemivashchenko96@gmail.com>
2022-02-16 16:43:11 +0300

    v 1.1

--- src/e12.java
+++ src/e12.java
@@ -7,18 +7,17 @@ public class e12 {
         Scanner in = new Scanner(System.in);
         System.out.print("Num: ");
         int a = in.nextInt();
+        int sum = 0;
+        int mult = 1;
         if(a < 0){
             a *= - 1; //делаем число положительным
         }
-        int num1 = -1, num2 = -1, num3 = -1;
-
-
-        for(int i = a; i > 0; i--){
-            if(i >= 100){
-
-            }
-
-
+        while (a > 0){
+            int num = a% 10;
+            sum += num;
+            mult *= num;
+            a /= 10;
         }
+        System.out.println("Sum of nums: " + sum);
     }
 }
